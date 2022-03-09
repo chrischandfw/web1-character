c97b76919622f0abf5e466e8ce0e804635ba98eb
chris_chan <chrischandfw@gmail.com>
2022-03-09 16:46:25 -0600

    set state in app js

--- app.js
+++ app.js
@@ -13,31 +13,37 @@ const catchphraseButton = document.getElementById('catchphrase-button');
 // set state for how many times the user changes the head, middle, and bottom
 // set state for all of the character's catchphrases
 
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
+
+const catchPhrase = [];
+
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-
+    const headSelect = headDropdown.value; 
     // increment the head change count state
-    
+    headCount++;
     // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `url(./assets/${headSelect}-head.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-
+    const middleSelect = middleDropdown.value;
     // increment the middle change count state
-    
+    middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url(./assets/${middleSelect}-middle.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-
+    const bottomSelect = bottomDropdown.value;
     // increment the bottom change count state
     
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
